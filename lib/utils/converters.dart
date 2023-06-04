26f256c71bebb17af8c1acf4f754950921aed22c
Mopilani <65030930+Mopilani@users.noreply.github.com>
2023-06-04 16:09:33 +0200

    Some Updates

--- lib/utils/converters.dart
+++ lib/utils/converters.dart
@@ -1,5 +1,9 @@
 
 String dateTimeToString(DateTime datetime) =>
     '${datetime.year}-${datetime.month}-${datetime.day} ${datetime.hour}:${datetime.minute}:${datetime.second}';
+String dateTimeToHexDecString(DateTime datetime) =>
+    '${(datetime.year).toRadixString(16)}-${datetime.month.toRadixString(16)}-${datetime.day.toRadixString(16)} ${datetime.hour.toRadixString(16)}:${datetime.minute.toRadixString(16)}:${datetime.second.toRadixString(16)}';
+String dateTimeToOctString(DateTime datetime) =>
+    '${(datetime.year).toRadixString(8)}-${datetime.month.toRadixString(8)}-${datetime.day.toRadixString(8)} ${datetime.hour.toRadixString(8)}:${datetime.minute.toRadixString(8)}:${datetime.second.toRadixString(8)}';
 String dateTimeToString2(DateTime datetime) =>
     '${datetime.year}-${datetime.month}-${datetime.day}-${datetime.hour}-${datetime.minute}-${datetime.second}';
--- lib/utils/system_db.dart
+++ lib/utils/system_db.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:mongo_dart/mongo_dart.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
+// import 'package:path_provider/path_provider.dart' as path_provider;
 
 import 'global_state.dart';
 import 'hcol.dart';
@@ -15,6 +15,7 @@ class SMDB extends SystemMDBService {
     String password = 'businet',
     bool auth = true,
     bool useMongo = true,
+    bool useMongoGridfs = false,
   }) : super(
           hiveDBPath: hiveDBPath,
           mongoDBUriString: mongoDBUriString,
@@ -22,6 +23,7 @@ class SMDB extends SystemMDBService {
           password: password,
           auth: auth,
           useMongo: useMongo,
+          useMongoGridfs: useMongoGridfs,
         );
 
   static Future<void> useMain() async {
@@ -56,8 +58,10 @@ class SystemMDBService {
     required this.mongoDBUriString,
     this.username = 'businet',
     this.password = 'businet',
+    this.docDir,
     this.auth = true,
     this.useMongo = false,
+    this.useMongoGridfs = false,
   });
 
   final String mongoDBUriString;
@@ -66,36 +70,58 @@ class SystemMDBService {
   final String password;
   final bool auth;
   final bool useMongo;
+  final bool useMongoGridfs;
+  final Directory? docDir;
 
   Db? _db;
+  Db? _GFSdb;
 
   Future<Db?> init() async {
     if (Platform.isAndroid && !useMongo) {
-      var docDir = await path_provider.getApplicationDocumentsDirectory();
-      var dbPath = docDir.path + '/' + hiveDBPath;
+      // var docDir = await path_provider.getApplicationDocumentsDirectory();
+      var dbPath = docDir!.path + '/' + hiveDBPath;
       _db = HDb(dbPath);
       print('Initializing HDB $dbPath');
     } else {
       _db = Db(mongoDBUriString);
+      _GFSdb = Db(mongoDBUriString + '-gfs');
       print('Initializing MDB $mongoDBUriString');
     }
 
     try {
       if (auth) {
+        await _db!.open();
         var authState = await _db!.authenticate(username, password);
-        if (authState) {
-          await _db!.open();
+
+        if (useMongoGridfs) {
+          await _GFSdb!.open();
+          var gFSDBauthState = await _GFSdb!.authenticate(username, password);
+          var collectionName = 'default-gfs-coll';
+          var gridFS = GridFS(_GFSdb!, collectionName);
+          print('GridFS Initialized');
+          GlobalState.set('gridFS', gridFS);
         } else {
           print('DB auth state: $authState');
         }
       } else {
         await _db!.open();
+        await _GFSdb!.open();
+
+        if (useMongoGridfs) {
+          var collectionName = 'default-gfs-coll';
+          var gridFS = GridFS(_GFSdb!, collectionName);
+          GlobalState.set('gridFS', gridFS);
+          print('GridFS Initialized');
+        }
       }
-    } catch (e) {
+    } catch (e, s) {
       print(BusinetDBError(e));
+      print(BusinetDBError(s));
       await _db!.open();
     }
     GlobalState.set('db2', _db);
+    GlobalState.set('GFSdb', _GFSdb);
+
     return _db;
   }
 
@@ -106,6 +132,7 @@ class SystemMDBService {
     password = 'businet',
     auth = true,
     useMongo = false,
+    useMongoGridfs = false,
   }) async {
     await SystemMDBService.db.close().then((value) async {
       await SystemMDBService(
@@ -115,6 +142,7 @@ class SystemMDBService {
         username: username,
         auth: auth,
         useMongo: useMongo,
+        useMongoGridfs: useMongoGridfs,
       ).init();
     });
   }
@@ -125,6 +153,7 @@ class SystemMDBService {
       mongoDBUriString: data['mongoDBUriString'],
       auth: data['auth'],
       useMongo: data['useMongo'],
+      useMongoGridfs: data['useMongoGridfs'],
       password: data['password'],
       username: data['username'],
     );
@@ -135,6 +164,7 @@ class SystemMDBService {
         'mongoDBUriString': mongoDBUriString,
         'auth': auth,
         'useMongo': useMongo,
+        'useMongoGridfs': useMongoGridfs,
         'password': password,
         'username': username,
       };
@@ -143,6 +173,14 @@ class SystemMDBService {
 
   static Db get db => () {
         return GlobalState.get('db2');
+      }();
+
+  static Db get gFSdb => () {
+        return GlobalState.get('GFSdb');
+      }();
+
+  static GridFS get gridFS => () {
+        return GlobalState.get('gridFS');
       }();
 }
 
